2609d499240ab905bec79662aa814c3d6ceed2b3
kaileighrose <kaileighrose@gmail.com>
2016-06-02 13:26:21 -0400

    update gemspec and publish to rubygems

--- lib/nyc_theater_discounts/show.rb
+++ lib/nyc_theater_discounts/show.rb
@@ -35,13 +35,5 @@ class NYCTheaterDiscounts::Show
   def find_by_name(nameval)
     self.all.detect{|a| a.name == nameval}
   end
-
-  def find_or_create_by_name(name)
-    if self.find_by_name(name) != nil
-      self.find_by_name(name)
-    else
-      self.create(name)
-    end
-  end
   
 end
